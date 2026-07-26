c70bf175dfdef476faddb0e34c2353195add2a07
cmorenoh99 <cmorenoh99@gmail.com>
2026-07-26 14:22:15 -0500

    scripts de asana sales-operations

---
+++ Utils/constants.py
@@ -0,0 +1,18 @@
+# ID PROYECTOS 
+ASANA_SALES_PROJECT_ID = '1216713295378538'
+ASANA_OPERATIONS_PROJECT_ID = '1216685996541408'
+
+
+## Options Field ASANA Estandar
+OPTS = {'opt_fields': "actual_time_minutes,approval_status,assignee,assignee.name,assignee.email,assignee_section,assignee_section.name,assignee_status,completed,completed_at,created_at,custom_fields.display_value,memberships,memberships.project,memberships.project.name,memberships.section,memberships.section.name,name,permalink_url,projects,projects.name,resource_subtype, modified_at, notes, subtasks"}
+OPTS_TIMES = {'opt_fields': "gid,attributable_to,attributable_to.name,created_by,created_by.name,duration_minutes,entered_on,offset,path,uri"}
+
+
+## BIBLIOTECA DE CUSTOM FIELDS ASANA
+ASANA_CUSTOM_FIELD_ID_ID='1216686175683670'
+ASANA_CUSTOM_FIELD_RESTAURANTE_ID='1216686175637759'
+ASANA_CUSTOM_FIELD_COUNTRY_ID='1216685996541449'
+ASANA_CUSTOM_FIELD_FECHA_VENTA_ID='1216718898681452'
+ASANA_CUSTOM_FIELD_RAZON_PERDIDA_ID='1216734626451947'
+ASANA_CUSTOM_FIELD_PRIORIDAD_ID='1216865710433395'
+ASANA_CUSTOM_FIELD_TIPO_ID='1216865881884967'
